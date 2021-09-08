963de86baed0eb8aad48e4af608af3ceebec7d91
unknown <trex33160@gmail.com>
2021-09-08 22:36:14 +0545

    added a function to fetch products from api

---
+++ src/containers/ProductComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const renderList = products.map((product) => {});
+  return (
+    <div className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"> </div>
+          <div className="content">
+            <div className="header">{}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProductComponent;
